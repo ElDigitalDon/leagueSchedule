c520c0092e1ccf5c5462fb388eb52861ce8f37b7
Donald Nolin <donald.nolin@ert.com>
2018-04-23 15:02:24 -0400

    UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {match} from "./models/match.interface";
+import {game} from "./models/game.interface";
+import {players} from "./models/players.inteface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  testPeople: string [] = [
+    "Matt",
+    "Don",
+    "Keith",
+    "Marah",
+    ""
+  ];
   title = 'app';
+  people: string [] = this.testPeople;
+  matchUp: match;
+
+  ngOnInit() {
+    this.matchUp = {games : []};
+    for (let i = 0; i < 5; i++ ) {
+      let players : players = {
+        player1 : "", player2 : ""
+      };
+      let game : game = { players };
+      this.matchUp.games.push(game);
+    }
+    this.generateSchedule(this.people);
+  }
+
+  add() {
+    this.people.push("");
+  }
+
+  addBlank(i) {
+    if (this.people.length - 1 === i ) {
+      this.add();
+    }
+  }
+
+  updatePerson($event, i) {
+    console.log($event.target.value);
+    if( $event.target.value.length !== 0) {
+      this.people[i] = $event.target.value;
+    }
+    console.dir(this.people);
+    this.generateSchedule(this.people.filter((person) => person.length !== 0 ));
+  }
+
+  generateSchedule(people) {
+    console.log("attempt to schedule");
+    if( this.people.length < 2) {
+      return;
+    }
+
+    let usages = {};
+    people.forEach( (person) =>  {
+      usages[person] = {timesUsed : 0, pairedWith : []};
+    });
+    this.matchUp.games.forEach( (game) => {
+      let nextSpouse = this.findNextLeastUsed( usages );
+      game.players.player1 = nextSpouse;
+      usages[nextSpouse].timesUsed++;
+      nextSpouse = this.findNextLeastUsed( usages, nextSpouse );
+      game.players.player2 = nextSpouse;
+      usages[nextSpouse].timesUsed++;
+      usages[game.players.player1].pairedWith.push(game.players.player2);
+      usages[game.players.player2].pairedWith.push(game.players.player1);
+      console.dir(usages);
+    });
+    console.dir(usages);
+  }
+
+  findNextLeastUsed( usages, firstSpouse = "" ) {
+    let curLeastUsed : number = -1;
+    let curLeastPerson : string;
+
+    for( let person in usages ) {
+      // Only search first party attributes and non-paired persons
+      if ( usages.hasOwnProperty(person)
+        && usages[person].pairedWith.includes(firstSpouse) === false
+        && person !== firstSpouse) {
+        // Found a person that hasn't been used?  Leave.  Now.
+        if ( usages[person].timesUsed === 0 ) {
+          return person;
+        }
+        // Save the least used person ... so far
+        if (curLeastUsed === -1 || usages[person].timesUsed < curLeastUsed) {
+          curLeastUsed = usages[person].timesUsed;
+          curLeastPerson = person;
+        }
+      }
+    }
+    return curLeastPerson;
+  }
 }
